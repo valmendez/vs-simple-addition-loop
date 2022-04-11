a05ee1eba41a2b9f0f8b0b365697e1f663f13aed
Valeria <valeriam.bonilla07@gmail.com>
2022-04-11 16:29:45 -0400

    Add project files.

---
+++ ProgramE/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnGetAnswer = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnGetAnswer
+        '
+        Me.btnGetAnswer.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGetAnswer.Location = New System.Drawing.Point(211, 40)
+        Me.btnGetAnswer.Name = "btnGetAnswer"
+        Me.btnGetAnswer.Size = New System.Drawing.Size(266, 71)
+        Me.btnGetAnswer.TabIndex = 0
+        Me.btnGetAnswer.Text = "Get Answer"
+        Me.btnGetAnswer.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 31
+        Me.ListBox1.Location = New System.Drawing.Point(211, 138)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(266, 190)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnGetAnswer)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGetAnswer As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ ProgramE/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub btnGetAnswer_Click(sender As Object, e As EventArgs) Handles btnGetAnswer.Click
+        Dim num As Integer = 2
+
+        Do
+
+            ListBox1.Items.Add(num & "    " & num + 2 & "    " & num + 4 & "    " & num + 6)
+            num = 10
+            ListBox1.Items.Add(num & " " & num + 2 & " " & num + 4 & " " & num + 6)
+            num = 18
+            ListBox1.Items.Add(num & " " & num + 2 & " " & num + 4 & " " & num + 6)
+            num = 26
+            ListBox1.Items.Add(num & " " & num + 2 & " " & num + 4 & " " & num + 6)
+
+        Loop Until (num <= 32)
+
+        '2 4 6 8
+        '10 12 14 16
+        '18 20 22 24
+        '26 28 30 32
+
+    End Sub
+End Class
